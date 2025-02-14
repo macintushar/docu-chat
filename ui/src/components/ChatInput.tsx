4ed695be185d960ec7fdb49c0c16e409700fe1c2
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-02-15 00:47:03 +0530

    updated chat style

--- ui/src/components/ChatInput.tsx
+++ ui/src/components/ChatInput.tsx
@@ -19,7 +19,7 @@ export default function ChatInput({
   setContextEnabled: (contextEnabled: boolean) => void;
 }) {
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-1 bg-zinc-950/50 rounded-lg">
+    <div className="flex flex-col items-center justify-center w-full gap-1 bg-zinc-100 dark:bg-zinc-950/50 rounded-lg">
       <Textarea
         name="input"
         id="chat-input"
