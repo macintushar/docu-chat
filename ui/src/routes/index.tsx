9a23307b7108120a1ed6bd102f87254bba478ece
Tushar Selvakumar <tusharkumar91111@gmail.com>
2025-02-16 22:58:44 +0530

    added basic Dashboard view

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,14 +1,6 @@
-import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import Dashboard from "@/views/Dashboard";
 
 export const Route = createFileRoute("/")({
-  component: HomeComponent,
+  component: Dashboard,
 });
-
-function HomeComponent() {
-  return (
-    <div>
-      <h3>Welcome Home!</h3>
-    </div>
-  );
-}
